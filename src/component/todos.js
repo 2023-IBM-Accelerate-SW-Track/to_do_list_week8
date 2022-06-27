3450e8f475a216f15e312e3b12ea588b2e923236
Kareithdyce <Kareith.dyce@gmail.com>
2022-06-27 12:44:35 -0700

    Update todos.js

--- src/component/todos.js
+++ src/component/todos.js
@@ -22,7 +22,7 @@ const Todos = ({ todos, deleteTodo }) => {
           <Card data-testid={todo.content} style={{marginTop:10, background: color}}>
             {/* Remember, we set the local state of this todo item when the user submits the form in 
             AddTodo.js. All we need to do is return the todo list item {todo.content} as well as its 
-            current date/time {todo.date}. Also, the item's id is utilized in order to correctly delete an item from the Todo list*/}.
+            current date/time {todo.date}. Also, the item's id is utilized in order to correctly delete an item from the Todo list*/}
             <ListItemButton component="a" href="#simple-list">
               <Checkbox style={{paddingLeft:0}} color="primary" onClick={() => deleteTodo(todo.id)}/>
               <ListItemText primary={todo.content} secondary={todo.duedate}/>
